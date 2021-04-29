d50f4bcf99616c13d4006c6d91a2425282c00a59
Gwang Sik <vrooming13>
2021-04-29 14:27:34 +0900

    04.29 Commit Test By OGS

---
+++ app/src/main/java/com/example/heallo/GitTestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.heallo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GitTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_test)
+    }
+}
