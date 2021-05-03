75fa54e8e22ed92e9549c3afa7a0baacad9da10f
vrooming13 <vrooming13@gmail.com>
2021-05-04 04:02:15 +0900

    게시글 작업중

---
+++ app/src/main/java/com/example/heallo/postActivity.kt
@@ -0,0 +1,11 @@
+package com.example.heallo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class postActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.postlayout)
+    }
+}
