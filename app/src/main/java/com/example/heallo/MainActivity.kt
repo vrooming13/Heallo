2c7ae67da71fefc3a818bdd7974956e204f7e590
vrooming13 <vrooming13@gmail.com>
2021-05-04 20:04:33 +0900

    게시글 작업중

--- app/src/main/java/com/example/heallo/MainActivity.kt
+++ app/src/main/java/com/example/heallo/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) { // 초기 앱 실행시 view 생성
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login) // xml 디자인 보여줌
+        setContentView(R.layout.postlayout) // xml 디자인 보여줌
     }
 }
