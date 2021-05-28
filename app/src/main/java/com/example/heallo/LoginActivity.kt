2918fbae6de7deec4f85eb11fcce288c7a6dc2a8
gwang <vrooming13@gmail.com>
2021-05-28 21:20:43 +0900

    05.28 Merge

--- app/src/main/java/com/example/heallo/LoginActivity.kt
+++ app/src/main/java/com/example/heallo/LoginActivity.kt
@@ -1,36 +1,99 @@
 package com.example.heallo
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import androidx.core.content.ContextCompat
-import androidx.core.text.set
-import androidx.core.text.toSpannable
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AppCompatActivity
+import com.cs.heallo.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private var firebaseAuth: FirebaseAuth? = null
+    private var firebaseAuthListener: FirebaseAuth.AuthStateListener? = null
+    private var input_email: EditText? = null
+    private var input_pwd: EditText? = null
+    private var login_btn: Button? = null
+    private var sign_up_btn: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        input_email = login_id
+        input_pwd = login_pwd
+        sign_up_btn = sign_up_btn1
 
-        login_btn.setOnClickListener{
-            val intent = Intent( this,HomeActivity::class.java)
-            startActivity(intent);
+        sign_up_btn!!.setOnClickListener {
+            fun onClick(v: View?) {
+                // SignUpActivity 연결
+                val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        login_btn = login_btn1
+        login_btn!!.setOnClickListener {
+            fun onClick(v: View?) {
+                if (input_email?.text.toString() != "" && input_pwd?.text
+                        .toString() != ""
+                ) {
+                    loginUser(
+                        input_email?.text.toString(),
+                        input_pwd?.text.toString()
+                    )
+                } else {
+                    Toast.makeText(this@LoginActivity, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show()
+                }
+            }
         }
 
-
-        sign_up_btn.setOnClickListener{
-            val intent2 = Intent( this,SignUpActivity::class.java)
-            startActivity(intent2);
-        }
-
-
+        FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user: FirebaseUser? = firebaseAuth.currentUser
+            if (user != null) {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+            }
+        }.also { firebaseAuthListener = it }
+    }
+    private fun loginUser(email: String?, password: String?) {
+        firebaseAuth?.signInWithEmailAndPassword(email.toString(), password.toString())
+            ?.addOnCompleteListener(this
+            ) { task ->
+                if (task.isSuccessful) {
+                    // 로그인 성공
+                    Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                    firebaseAuthListener?.let { firebaseAuth?.addAuthStateListener(it) }
+                } else {
+                    // 로그인 실패
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "아이디 또는 비밀번호가 일치하지 않습니다.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 
+    override fun onStart() {
+        super.onStart()
+    }
 
-
+    override fun onStop() {
+        super.onStop()
+        if (firebaseAuthListener != null) {
+            firebaseAuth?.removeAuthStateListener(firebaseAuthListener!!)
+        }
+    }
 }
--- app/src/main/java/com/example/heallo/SignUpActivity.kt
+++ app/src/main/java/com/example/heallo/SignUpActivity.kt
@@ -1,22 +1,72 @@
 package com.example.heallo
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.cs.heallo.R
 
-// 메인액티비티
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 class SignUpActivity : AppCompatActivity() {
     //뷰가 생성되었을 때
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var firebaseAuth: FirebaseAuth? = null
+    private var input_id: EditText? = null
+    private var input_pwd: EditText? = null
+    private var check_pwd: EditText? = null
+    private var input_name: EditText? = null
+    private var buttonJoin: Button? = null
+    private var loginBtn: Button? = null
 
-        //그릴 xml 뷰 파일을 연결 시켜준다. 즉, 설정한다. r.layout = res.layout
-        setContentView(R.layout.activity_sign_up)
-        val sign_up_btn2: Button = findViewById(R.id.sign_up_btn2)
-        sign_up_btn2.setOnClickListener { //회원가입 버튼 클릭시
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+    private fun onClick() {
+        // SignUpActivity 연결
+        if (input_id?.text.toString() != "" && input_pwd?.text
+                .toString().isNullOrEmpty()
+        ) {
+            // 이메일과 비밀번호가 공백이 아닌 경우
+            createUser(
+                input_id?.text.toString(),
+                input_pwd?.text.toString(),
+                input_name?.text.toString()
+            )
+        } else {
+            // 이메일과 비밀번호가 공백인 경우
+            Toast.makeText(this@SignUpActivity, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_LONG)
+                .show()
         }
+    }
+    protected override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        input_id = su_id
+        input_pwd = su_pwd
+        check_pwd = su_pwd2
+        input_name = su_name
+        loginBtn = login_btn1
+        buttonJoin = sign_up_btn2
+
+        buttonJoin!!.setOnClickListener {
+            onClick()
+        }
+    }
+
+    private fun createUser(email: String, password: String, name: String) {
+        firebaseAuth?.createUserWithEmailAndPassword(email, password)
+            ?.addOnCompleteListener(this
+            ) { task ->
+                if (task.isSuccessful) {
+                    // 회원가입 성공시
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                    finish()
+                } else {
+                    // 계정이 중복된 경우
+                    Toast.makeText(this, "이미 존재하는 계정입니다.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
     }
 }
