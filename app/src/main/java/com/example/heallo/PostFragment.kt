ca2c03ef4628ee4aa63c78d6b8ccb259a46ef717
NAMJEONGHYEOK <namjh654@naver.com>
2021-11-05 15:19:30 +0900

    post_fragment 권한 설정 변경

--- app/src/main/java/com/example/heallo/PostFragment.kt
+++ app/src/main/java/com/example/heallo/PostFragment.kt
@@ -3,6 +3,8 @@ package com.example.heallo
 import ListAdapter
 import ResultSearchKeyword
 import android.Manifest
+import android.Manifest.*
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
@@ -12,7 +14,9 @@ import android.location.LocationManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.provider.MediaStore
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -22,6 +26,7 @@ import android.widget.EditText
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.finishAffinity
 import androidx.core.content.ContextCompat
@@ -61,7 +66,7 @@ class PostFragment : Fragment() {
     private var mapView : net.daum.mf.map.api.MapView? =null
     //kakao map api permission
     val PERMISSIONS_REQUEST_CODE = 100
-    var REQUIRED_PERMISSIONS = arrayOf<String>( Manifest.permission.ACCESS_FINE_LOCATION)
+    var REQUIRED_PERMISSIONS = arrayOf(permission.ACCESS_FINE_LOCATION)
     val PERM_STORAGE = 102
 
     companion object {
@@ -110,86 +115,8 @@ class PostFragment : Fragment() {
         // mapview 생성
          mapView = net.daum.mf.map.api.MapView(activity)
 
+        map()
 
-        val permissionCheck = ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
-        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
-            val lm: LocationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            try {
-
-                val recyclerView: RecyclerView = rootView!!.rvList
-                recyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
-                recyclerView.adapter = listAdapter
-
-
-                //선택시 이벤트 처리
-                listAdapter.setItemClickListener(object: ListAdapter.OnItemClickListener {
-                    override fun onClick(v: View, position: Int) {
-                        //좌표 지정.
-                        val mapPoint = MapPoint.mapPointWithGeoCoord(listItems[position].y, listItems[position].x)
-                        mapView!!.setMapCenterPointAndZoomLevel(mapPoint, 1, true)
-                        //rootView!!.searchView.setQuery("",false)
-                        // 검색 리싸이클러뷰 닫기.
-                        recyclerView.visibility= GONE
-                        // 위도경도 저장.
-                        uLatitude = listItems[position].y
-                        uLongitude = listItems[position].x
-                        addresses = listItems[position].address
-
-                        //검색바 텍스트 선택 내용으로 변경.
-                        rootView?.searchView?.setQuery( listItems[position].name,false)
-                        //터치 후 항상 닫기.
-                        rootView!!.rvList.setVisibility(View.GONE);
-                        //터치 후 키보드 내리기
-                        hideKeyboard()
-
-                    }
-                })
-
-                val userNowLocation: Location =
-                    lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!! // 유저 현재위치 저장.
-                 uLatitude = userNowLocation.latitude  // 사용자 위도
-                 uLongitude = userNowLocation.longitude // 사용자 경도
-                val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!) // 유저 현재 위치 저장.
-                // 중심점 변경.
-                mapView!!.setMapCenterPointAndZoomLevel(uNowPosition,2,true) //  kakao map 위도,경도를 통한 현재 위치 지정.
-                //줌 컨트롤러
-
-                //내 위치마커
-                var defaultmaker = MapPOIItem()
-                defaultmaker.apply {
-                    itemName="현재 위치"
-                    mapPoint = MapPoint.mapPointWithGeoCoord(uLatitude!!,uLongitude!!)
-                    markerType = MapPOIItem.MarkerType.BluePin
-//                    selectedMarkerType = MapPOIItem.MarkerType.CustomImage  // 클릭 시 마커 모양 (커스텀)
-//                    customSelectedImageResourceId = R.drawable.이미지       // 클릭 시 커스텀 마커 이미지
-//                    isCustomImageAutoscale = false      // 커스텀 마커 이미지 크기 자동 조정
-//                    setCustomImageAnchor(0.5f, 1.0f)    // 마커 이미지 기준점
-                }
-
-                mapView!!.addPOIItem(defaultmaker)
-
-
-
-            }catch(e: NullPointerException){
-                Log.e("LOCATION_ERROR", e.toString())
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    Log.d("test","1")
-//                    ActivityCompat.finishAffinity(this)
-                }else{
-                    Log.d("test","2")
-
-//                    ActivityCompat.finishAffinity(this)
-                }
-                Log.d("test","3")
-
-//                val intent = Intent(this, MainActivity::class.java)
-//                startActivity(intent)
-//                System.exit(0)
-            }
-        }else{
-            Toast.makeText(activity, "위치 권한이 없습니다.", Toast.LENGTH_SHORT).show()
-            ActivityCompat.requestPermissions(requireActivity(), REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE )
-        }
 //        서치뷰 검색처리.
         rootView!!.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
@@ -223,11 +150,7 @@ class PostFragment : Fragment() {
 
 
         rootView!!.imageView.setOnClickListener {
-            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERM_STORAGE)
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.type = MediaStore.Images.Media.CONTENT_TYPE
-            startActivityForResult(intent, PERM_STORAGE)
-            onActivityResult(PERM_STORAGE, RESULT_OK, intent)
+            requestPermissions()
         }
 
         rootView!!.writeBtn.setOnClickListener {
@@ -243,11 +166,76 @@ class PostFragment : Fragment() {
 
 
 
-
-
-
-
         return rootView!!.root
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ): Unit {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            100 -> {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Log.d("test","100 c")
+//                    승인 결과처리 맞으면 map 실행
+//                    map()
+                } else {
+
+                    //재요청
+                    if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){
+                        Log.d("test","1")
+                    } else {
+                        Log.d("test","2")
+
+                        var alertDialog = AlertDialog.Builder(requireActivity())
+                        alertDialog.setTitle("위치권한 필요")
+                        alertDialog.setMessage("앱 사용을 위해서는 위치 정보 권한이 필요합니다. 설정 화면으로 이동하시겠습니까?")
+                        alertDialog.setPositiveButton(
+                            "확인"
+                        ) { dialogInterface, i ->
+                            // 거부 및 다시보지 않기 선택시 직접적인 락 해제를 해줘야한다.
+                            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                .setData(Uri.parse("package:" + BuildConfig.APPLICATION_ID))
+                            startActivity(intent)
+                        }
+                        alertDialog.setNegativeButton("취소", { dialogInterface, i -> dialogInterface.dismiss() })
+                        alertDialog.show()
+                    }
+                }
+            }
+            102 -> {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    val intent = Intent(Intent.ACTION_PICK)
+                    intent.type = MediaStore.Images.Media.CONTENT_TYPE
+                    startActivityForResult(intent, PERM_STORAGE)
+                    onActivityResult(PERM_STORAGE, RESULT_OK, intent)
+                } else {
+                    if (shouldShowRequestPermissionRationale(
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                    ) {
+                        Log.d("test", "1")
+                    } else {
+                        Log.d("test", "2")
+                        var alertDialog = AlertDialog.Builder(requireActivity())
+                        alertDialog.setTitle("외부 저장소 접근 권한 필요")
+                        alertDialog.setMessage("앱 사용을 위해서는 외부 저장소 접근 권한이 필요합니다. 설정 화면으로 이동하시겠습니까?")
+                        alertDialog.setPositiveButton(
+                            "확인"
+                        ) { dialogInterface, i ->
+                            // 거부 및 다시보지 않기 선택시 직접적인 락 해제를 해줘야한다.
+                            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                .setData(Uri.parse("package:" + BuildConfig.APPLICATION_ID))
+                            startActivity(intent)
+                        }
+                        alertDialog.setNegativeButton("취소", { dialogInterface, i -> dialogInterface.dismiss() })
+                        alertDialog.show()
+                    }
+                }
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -268,6 +256,100 @@ class PostFragment : Fragment() {
 
 
 
+    private fun map() {
+        Log.d("test","123")
+        val permissionCheck = ContextCompat.checkSelfPermission(
+            requireActivity(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            val lm: LocationManager =
+                requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            try {
+
+                val recyclerView: RecyclerView = rootView!!.rvList
+                recyclerView.layoutManager =
+                    LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+                recyclerView.adapter = listAdapter
+
+
+                //선택시 이벤트 처리
+                listAdapter.setItemClickListener(object : ListAdapter.OnItemClickListener {
+                    override fun onClick(v: View, position: Int) {
+                        //좌표 지정.
+                        val mapPoint = MapPoint.mapPointWithGeoCoord(
+                            listItems[position].y,
+                            listItems[position].x
+                        )
+                        mapView!!.setMapCenterPointAndZoomLevel(mapPoint, 1, true)
+                        //rootView!!.searchView.setQuery("",false)
+                        // 검색 리싸이클러뷰 닫기.
+                        recyclerView.visibility = GONE
+                        // 위도경도 저장.
+                        uLatitude = listItems[position].y
+                        uLongitude = listItems[position].x
+                        addresses = listItems[position].address
+
+                        //검색바 텍스트 선택 내용으로 변경.
+                        rootView?.searchView?.setQuery(listItems[position].name, false)
+                        //터치 후 항상 닫기.
+                        rootView!!.rvList.setVisibility(View.GONE);
+                        //터치 후 키보드 내리기
+                        hideKeyboard()
+
+                    }
+                })
+
+                val userNowLocation: Location =
+                    lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!! // 유저 현재위치 저장.
+                uLatitude = userNowLocation.latitude  // 사용자 위도
+                uLongitude = userNowLocation.longitude // 사용자 경도
+                val uNowPosition =
+                    MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!) // 유저 현재 위치 저장.
+                // 중심점 변경.
+                mapView!!.setMapCenterPointAndZoomLevel(
+                    uNowPosition,
+                    2,
+                    true
+                ) //  kakao map 위도,경도를 통한 현재 위치 지정.
+                //줌 컨트롤러
+
+                //내 위치마커
+                var defaultmaker = MapPOIItem()
+                defaultmaker.apply {
+                    itemName = "현재 위치"
+                    mapPoint = MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!)
+                    markerType = MapPOIItem.MarkerType.BluePin
+//                    selectedMarkerType = MapPOIItem.MarkerType.CustomImage  // 클릭 시 마커 모양 (커스텀)
+//                    customSelectedImageResourceId = R.drawable.이미지       // 클릭 시 커스텀 마커 이미지
+//                    isCustomImageAutoscale = false      // 커스텀 마커 이미지 크기 자동 조정
+//                    setCustomImageAnchor(0.5f, 1.0f)    // 마커 이미지 기준점
+                }
+
+                mapView!!.addPOIItem(defaultmaker)
+
+
+            } catch (e: NullPointerException) {
+                Log.e("LOCATION_ERROR", e.toString())
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    Log.d("test", "1")
+//                    ActivityCompat.finishAffinity(this)
+                } else {
+                    Log.d("test", "2")
+
+//                    ActivityCompat.finishAffinity(this)
+                }
+                Log.d("test", "3")
+
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+//                System.exit(0)
+            }
+        } else {
+            Toast.makeText(activity, "위치 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+          requestPermissions(REQUIRED_PERMISSIONS,PERMISSIONS_REQUEST_CODE)
+        }
+    }
 
     private fun contentUpload() {
         var content = ContentDTO()
@@ -327,6 +409,9 @@ class PostFragment : Fragment() {
         }
 
     }
+
+
+
     // 키워드 검색 함수
     private fun searchKeyword(keyword: String) {
         val retrofit = Retrofit.Builder()   // Retrofit 구성
@@ -387,6 +472,24 @@ class PostFragment : Fragment() {
         // else 일 경우 : 검색결과 없을 경우 알림 삭제. autoCompleteText
     }
 
+    private fun requestPermissions() {
+        var permissionCheck = ContextCompat.checkSelfPermission(requireActivity(), permission.READ_EXTERNAL_STORAGE)
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(
+                arrayOf(permission.READ_EXTERNAL_STORAGE),PERM_STORAGE)
+        } else {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = MediaStore.Images.Media.CONTENT_TYPE
+            startActivityForResult(intent, PERM_STORAGE)
+            onActivityResult(PERM_STORAGE, RESULT_OK, intent)
+        }
+    }
+
+
+
+
+
+
     private fun hideKeyboard(){
         val inputManager =
             requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -394,6 +497,8 @@ class PostFragment : Fragment() {
             requireActivity().currentFocus?.windowToken,0
         )
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
